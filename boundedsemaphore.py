64312ffe3a9b877304a8790a86da7de817adfe2e
Chenjie <chenjieh@ucm.es>
2022-02-17 10:38:47 +0100

    upload boundedsemaphore.py

---
+++ boundedsemaphore.py
@@ -0,0 +1,48 @@
+from multiprocessing import Process
+from multiprocessing import current_process
+from multiprocessing import Value, Array
+from multiprocessing import BoundedSemaphore
+
+N = 8
+
+def task(s, common, tid):
+	a = 0
+	for i in range(100):
+
+		print(f'{tid}-{i}:Non-critical Section')
+		a += 1
+		print(f'{tid}-{i}:End of non-critical Section')
+		
+		s.acquire()
+		try:
+			print(f'{tid}-{i}:Critical Section')
+			v = common.value + 1
+			print(f'{tid}-{i}:Inside Critical Section')
+			common.value = v
+			print(f'{tid}-{i}:End of Critical Section')
+		finally:
+			s.release()
+		
+
+
+def main():
+	lp = []
+	common = Value('i', 0)
+	semaphore = BoundedSemaphore(1)
+	
+	for tid in range(N):
+		lp.append(Process(target=task, args = (semaphore, common, tid)))
+	
+	print(f"Valor inicial del contador {common.value}")
+	for p in lp:
+		p.start()
+	
+	for p in lp:
+		p.join()
+	
+	print(f"Valor final del contador {common.value}")
+	print("fin")
+	
+if __name__ == "__main__":
+	main()
+
